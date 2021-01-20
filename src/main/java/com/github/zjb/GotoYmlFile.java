45f9b2c5f76a03670663bb0c8b9a85a02d3283ed
zhaojingbo <wb_zhaojingbo@kuaishou.com>
2021-01-20 18:12:53 +0800

    5.0支持yml to java

--- src/main/java/com/github/zjb/GotoYmlFile.java
+++ src/main/java/com/github/zjb/GotoYmlFile.java
@@ -1,7 +1,11 @@
 package com.github.zjb;
 
 import com.github.zjb.util.EnumUtil;
+import com.google.common.collect.Lists;
 import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler;
+import com.intellij.ide.highlighter.JavaFileType;
+import com.intellij.lang.jvm.annotation.JvmAnnotationAttribute;
+import com.intellij.lang.jvm.annotation.JvmAnnotationAttributeValue;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
@@ -10,16 +14,23 @@ import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.PsiJavaFileImpl;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.JavaMatchers;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.yaml.YAMLFileType;
+import org.jetbrains.yaml.YAMLLanguage;
 import org.jetbrains.yaml.YAMLUtil;
 import org.jetbrains.yaml.psi.YAMLFile;
+import org.jetbrains.yaml.psi.YAMLPsiElement;
+import org.jetbrains.yaml.psi.impl.YAMLPlainTextImpl;
 
 import java.util.*;
 
@@ -30,12 +41,61 @@ import java.util.*;
 
 public class GotoYmlFile implements GotoDeclarationHandler {
 
+    private static final PsiElement[] DEFAULT_RESULT = new PsiElement[0];
+
+
     @Nullable
     @Override
     public PsiElement[] getGotoDeclarationTargets(@Nullable PsiElement sourceElement, int offset, Editor editor) {
-        if (!(sourceElement instanceof PsiJavaToken)) {
-            return new PsiElement[0];
+        if (sourceElement instanceof PsiJavaToken) {
+            return javaGoToYml(sourceElement);
         }
+        if (sourceElement.getLanguage().is(YAMLLanguage.INSTANCE)) {
+            return ymlToJava(sourceElement);
+        }
+        return DEFAULT_RESULT;
+    }
+
+    private PsiElement[] ymlToJava(@NotNull PsiElement sourceElement) {
+        String configFullName = YAMLUtil.getConfigFullName(PsiTreeUtil.getParentOfType(sourceElement, YAMLPsiElement.class));
+        Pair<PsiElement, String> value = YAMLUtil.getValue((YAMLFile) sourceElement.getContainingFile(), configFullName.split("\\."));
+        if (Objects.isNull(value) || !(value.getFirst() instanceof YAMLPlainTextImpl)) {
+            return DEFAULT_RESULT;
+        }
+        Project project = sourceElement.getProject();
+        Collection<VirtualFile> files = FileTypeIndex.getFiles(JavaFileType.INSTANCE, GlobalSearchScope.projectScope(project));
+        if (CollectionUtils.isEmpty(files)) {
+            return DEFAULT_RESULT;
+        }
+        ArrayList<PsiElement> result = Lists.newArrayList();
+        for (VirtualFile file : files) {
+            Collection<PsiAnnotation> childrenOfType = PsiTreeUtil.findChildrenOfType(PsiManager.getInstance(project).findFile(file), PsiAnnotation.class);
+            if (CollectionUtils.isEmpty(childrenOfType)) {
+                continue;
+            }
+
+            for (PsiAnnotation psiAnnotation : childrenOfType) {
+                if (!EnumUtil.containAnnotation(psiAnnotation)) {
+                    continue;
+                }
+                PsiNameValuePair[] attributes1 = psiAnnotation.getParameterList().getAttributes();
+                for (PsiNameValuePair psiNameValuePair : attributes1) {
+                    String literalValue = psiNameValuePair.getLiteralValue();
+                    if (StringUtils.isBlank(literalValue)) {
+                        continue;
+                    }
+
+                    String valueKey = literalValue.substring(literalValue.indexOf("${") + 2, literalValue.indexOf("}"));
+                    if (Objects.equals(valueKey, configFullName)) {
+                        result.add(psiAnnotation);
+                    }
+                }
+            }
+        }
+        return result.toArray(new PsiElement[result.size()]);
+    }
+
+    private PsiElement[] javaGoToYml(@NotNull PsiElement sourceElement) {
         IElementType tokenType = ((PsiJavaToken) sourceElement).getTokenType();
         if (tokenType != JavaTokenType.STRING_LITERAL) {
             return new PsiElement[0];
