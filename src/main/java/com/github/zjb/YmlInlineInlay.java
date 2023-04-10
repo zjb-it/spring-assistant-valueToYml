9fefbeae35c2ffce371d4ee10f2acfa67f889763
zhaojingbo <zjbhnay@163.com>
2023-04-10 14:33:25 +0800

    优化 hint

--- src/main/java/com/github/zjb/YmlInlineInlay.java
+++ src/main/java/com/github/zjb/YmlInlineInlay.java
@@ -1,61 +1,42 @@
 package com.github.zjb;
 
 import com.github.zjb.setting.AppSettingsState;
+import com.intellij.codeInsight.daemon.impl.EditorTracker;
+import com.intellij.codeInsight.daemon.impl.EditorTrackerListener;
 import com.intellij.codeInsight.daemon.impl.HintRenderer;
+import com.intellij.ide.highlighter.JavaFileType;
+import com.intellij.lang.LanguageUtil;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.InlayModel;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.FileEditorManagerListener;
+import com.intellij.openapi.editor.*;
+import com.intellij.openapi.editor.actionSystem.EditorActionManager;
+import com.intellij.openapi.fileEditor.*;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectLocator;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.newvfs.BulkFileListener;
+import com.intellij.openapi.vfs.newvfs.events.VFileContentChangeEvent;
+import com.intellij.openapi.vfs.newvfs.events.VFileCreateEvent;
+import com.intellij.openapi.vfs.newvfs.events.VFileDeleteEvent;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.psi.*;
+import com.intellij.psi.util.FileTypeUtils;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.util.PsiUtilCore;
 import org.apache.commons.collections.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.notebooks.visualization.r.inlays.EditorInlaysManager;
+import org.jetbrains.yaml.YAMLFileType;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
-public class YmlInlineInlay implements FileEditorManagerListener {
+public class YmlInlineInlay implements BulkFileListener, EditorTrackerListener {
 
     private static final Logger LOG = Logger.getInstance(YmlInlineInlay.class);
-
-    @Override
-    public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
-
-        if (!AppSettingsState.getInstance().hintCheckbox) {
-            return;
-        }
-        try {
-            Project project = source.getProject();
-            DumbService.getInstance(project).smartInvokeLater(() -> {
-                PsiFile psiFile = PsiUtilCore.getPsiFile(project, file);
-                Collection<PsiAnnotation> psiElements = PsiTreeUtil.findChildrenOfAnyType(psiFile, PsiAnnotation.class);
-                if (CollectionUtils.isNotEmpty(psiElements)) {
-                    Editor editor = source.getSelectedTextEditor();
-                    InlayModel inlayModel = editor.getInlayModel();
-                    if (Objects.isNull(inlayModel)) {
-                        return;
-                    }
-                    psiElements.forEach(element -> {
-                        if (AppSettingsState.getInstance().ANNOTATIONS.contains(element.getQualifiedName())) {
-                            @NotNull PsiElement[] ymlPsiElements = GotoYmlFile.getYmlPsiElements(PsiTreeUtil.findChildOfType(element, PsiLiteralExpression.class));
-                            for (PsiElement ymlFile : ymlPsiElements) {
-                                inlayModel.addInlineElement(element.getTextOffset() + element.getTextLength(), new HintRenderer(getEnv(ymlFile.getContainingFile().getName()) + " : " + ymlFile.getLastChild().getText()));
-                            }
-                        }
-                    });
-                }
-            });
-
-        } catch (PsiInvalidElementAccessException e) {
-            LOG.error(e);
-        }
-    }
-
 
 
     public static String getEnv(String ymlFileName) {
@@ -65,4 +46,60 @@ public class YmlInlineInlay implements FileEditorManagerListener {
         return "default";
     }
 
+    @Override
+    public void after(@NotNull List<? extends @NotNull VFileEvent> events) {
+        for (VFileEvent event : events) {
+            VirtualFile file = event.getFile();
+            if (file.getFileType() instanceof YAMLFileType || file.getFileType() instanceof JavaFileType) {
+                Project project = ProjectLocator.getInstance().guessProjectForFile(file);
+                List<? extends Editor> activeEditors = EditorTracker.getInstance(project).getActiveEditors();
+                for (Editor activeEditor : activeEditors) {
+                    showInlay(activeEditor, FileDocumentManager.getInstance().getFile(activeEditor.getDocument()), project);
+                }
+            }
+        }
+
+
+    }
+
+    @Override
+    public void activeEditorsChanged(@NotNull List<? extends Editor> activeEditors) {
+        for (Editor editor : activeEditors) {
+            VirtualFile file = FileDocumentManager.getInstance().getFile(editor.getDocument());
+            try {
+                Project project = editor.getProject();
+                showInlay(editor, file, project);
+            } catch (PsiInvalidElementAccessException e) {
+                LOG.error(e);
+            }
+        }
+    }
+
+    private static void showInlay(Editor editor, VirtualFile file, Project project) {
+        DumbService.getInstance(project).smartInvokeLater(() -> {
+            PsiFile psiFile = PsiUtilCore.getPsiFile(project, file);
+            if (!(psiFile.getFileType() instanceof JavaFileType)) {
+                return;
+            }
+
+            Collection<PsiAnnotation> psiElements = PsiTreeUtil.findChildrenOfAnyType(psiFile, PsiAnnotation.class);
+            if (CollectionUtils.isEmpty(psiElements)) {
+                return;
+            }
+            InlayModel inlayModel = editor.getInlayModel();
+            if (Objects.isNull(inlayModel)) {
+                return;
+            }
+            psiElements.forEach(element -> {
+                if (AppSettingsState.getInstance().ANNOTATIONS.contains(element.getQualifiedName())) {
+                    TextRange textRange = element.getTextRange();
+                    inlayModel.getInlineElementsInRange(textRange.getStartOffset(), textRange.getEndOffset(), HintRenderer.class).forEach(Inlay::dispose);
+                    @NotNull PsiElement[] ymlPsiElements = GotoYmlFile.getYmlPsiElements(PsiTreeUtil.findChildOfType(element, PsiLiteralExpression.class));
+                    for (PsiElement ymlFile : ymlPsiElements) {
+                        inlayModel.addInlineElement(element.getTextOffset() + element.getTextLength(), new HintRenderer(getEnv(ymlFile.getContainingFile().getName()) + " : " + ymlFile.getLastChild().getText()));
+                    }
+                }
+            });
+        });
+    }
 }
